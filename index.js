a118e77d498a581455d7e069f36d01f5a89f0817
ucolinmee <colinjmeehan23@gmail.com>
2024-02-08 19:30:45 +0800

    Add buttons selection for game and logic event listeners to detect which selection was made by user

--- index.js
+++ index.js
@@ -29,32 +29,39 @@ function playRound(c1, c2) {
     }
 }
 
-function playGame() {
-    var userScore = 0;
-    var computerScore = 0;
-    for (var i=1; i<6; i++) {
-        console.log("Round " + i + ", User score: " + userScore + "| Computer score: " + computerScore);
-        var userSelection = prompt("Enter your choice (rock, paper, scissors): ");
-        var computerSelection = getComputerChoice();
-        var result = playRound(userSelection, computerSelection);
-        if (result > 0) {
-            console.log("You won. " + userSelection + " beats " + computerSelection);
-            userScore++;
-        } else if (result < 0) {
-            console.log("You lost. " + computerSelection + " beats " + userSelection);
-            computerScore++;
-        } else {
-            console.log("Draw. You both chose " + computerSelection);
-        }
-    }
-    if (userScore > computerScore) {
-        console.log("Congratulations you won!");
-    } else if (computerScore > userScore) {
-        console.log("Aww it's alright try again!");
-    } else {
-        console.log("Wow you are as smart as a computer!");
-    }
-    console.log("User score: " + userScore + "| Computer score: " + computerScore + ". Thanks for playing!");
-}
+var selections = document.querySelectorAll("img");
+selections.forEach((selection) => {
+    selection.addEventListener("click", (e) => {
+        console.log(e.target.id);
+    })
+})
 
-playGame();
+// function playGame() {
+//     var userScore = 0;
+//     var computerScore = 0;
+//     for (var i=1; i<6; i++) {
+//         console.log("Round " + i + ", User score: " + userScore + "| Computer score: " + computerScore);
+//         var userSelection = prompt("Enter your choice (rock, paper, scissors): ");
+//         var computerSelection = getComputerChoice();
+//         var result = playRound(userSelection, computerSelection);
+//         if (result > 0) {
+//             console.log("You won. " + userSelection + " beats " + computerSelection);
+//             userScore++;
+//         } else if (result < 0) {
+//             console.log("You lost. " + computerSelection + " beats " + userSelection);
+//             computerScore++;
+//         } else {
+//             console.log("Draw. You both chose " + computerSelection);
+//         }
+//     }
+//     if (userScore > computerScore) {
+//         console.log("Congratulations you won!");
+//     } else if (computerScore > userScore) {
+//         console.log("Aww it's alright try again!");
+//     } else {
+//         console.log("Wow you are as smart as a computer!");
+//     }
+//     console.log("User score: " + userScore + "| Computer score: " + computerScore + ". Thanks for playing!");
+// }
+// 
+// playGame();
