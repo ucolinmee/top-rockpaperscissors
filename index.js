634948f77d8217c5cd71c5fa3a83e892876c59d4
ucolinmee <colinjmeehan23@gmail.com>
2024-02-08 23:56:08 +0800

    Keep track of score and announce winner once someone reaches 5 points

--- index.js
+++ index.js
@@ -7,68 +7,76 @@ function playRound(user, comp) {
     comp = comp.toLowerCase();
     user = user.toLowerCase();
     var message = "";
+    var winner;
     if (user === comp) {
-        message = "Draw. You both chose " + c1;
+        message = "Draw. You both chose " + user;
+        winner = 0;
     } else if (user === "rock") {
         if (comp === "paper") {
-            message = "You lost. " + computerSelection + " beats " + userSelection;
+            message = "You lost. " + comp + " beats " + user;
+            winner = -1;
         } else {
             message = "You won. " + user + " beats " + comp;
+            winner = 1;
         }
     } else if (user === "paper") {
         if (comp === "scissors") {
-            message = "You lost. " + computerSelection + " beats " + userSelection;
+            message = "You lost. " + comp + " beats " + user;
+            winner = -1;
         } else {
             message = "You won. " + user + " beats " + comp;
+            winner = 1;
         }
     } else if (user === "scissors") {
         if (comp === "rock") {
-            message = "You lost. " + computerSelection + " beats " + userSelection;
+            message = "You lost. " + comp + " beats " + user;
+            winner = -1;
         } else {
             message = "You won. " + user + " beats " + comp;
+            winner = 1;
         }
     } 
     displayResult(message);
+    return winner;
 }
-
-var selections = document.querySelectorAll("img");
-selections.forEach((selection) => {
-    selection.addEventListener("click", (e) => {
-        console.log(e.target.id);
-    })
-})
 
 function displayResult(msg) {
     var result = document.querySelector(".result");
     result.textContent = msg;
 }
 
-// function playGame() {
-//     var userScore = 0;
-//     var computerScore = 0;
-//     for (var i=1; i<6; i++) {
-//         console.log("Round " + i + ", User score: " + userScore + "| Computer score: " + computerScore);
-//         var userSelection = prompt("Enter your choice (rock, paper, scissors): ");
-//         var computerSelection = getComputerChoice();
-//         var result = playRound(userSelection, computerSelection);
-//         if (result > 0) {
-//             console.log("You won. " + userSelection + " beats " + computerSelection);
-//             userScore++;
-//         } else if (result < 0) {
-//             console.log("You lost. " + computerSelection + " beats " + userSelection);
-//             computerScore++;
-//         } else {
-//             console.log("Draw. You both chose " + computerSelection);
-//         }
-//     }
-//     if (userScore > computerScore) {
-//         console.log("Congratulations you won!");
-//     } else if (computerScore > userScore) {
-//         console.log("Aww it's alright try again!");
-//     } else {
-//         console.log("Wow you are as smart as a computer!");
-//     }
-//     console.log("User score: " + userScore + "| Computer score: " + computerScore + ". Thanks for playing!");
-// }
-// 
-// playGame();
+function updateScoreboard(user, comp) {
+    var userScore = document.querySelector(".user-score");
+    userScore.textContent = "User score: " + user;
+
+    var compScore = document.querySelector(".computer-score");
+    compScore.textContent = "Computer score: " + comp;
+}
+
+function playGame() {
+    var userScore = 0;
+    var computerScore = 0;
+    var selections = document.querySelectorAll("img");
+    selections.forEach((selection) => {
+        selection.addEventListener("click", (e) => {
+            var winner = playRound(e.target.id, getComputerChoice());
+            if (winner > 0) {
+                userScore++;
+            } else if (winner < 0) {
+                computerScore++;
+            }
+            updateScoreboard(userScore, computerScore);
+            if (userScore >= 5 || computerScore >= 5) {
+                var winnerMessage = "";
+                if (userScore >= 5) {
+                    winnerMessage = "Congratulations! You won the game.";
+                } else if (computerScore >= 5) {
+                    winnerMessage = "Uh oh you lost, but thanks for trying."
+                }
+                displayResult(winnerMessage);
+            }
+        })
+    })
+}
+
+playGame();
